b972dbc4b7cc8a05b3047d8c282be74adfe4e3e9
ManoelLimaDev <kkmanel.lima@gmail.com>
2026-01-30 19:02:47 -0300

    feat: add staticmodifier package and static modifier

---
+++ src/br/com/manoellimadev/javastudying/oop/staticmodifier/domain/Account.java
@@ -0,0 +1,54 @@
+package br.com.manoellimadev.javastudying.oop.staticmodifier.domain;
+
+public class Account {
+    public static double transfLimit = 1500.00;
+    private double balance;
+    private String name;
+    private String personId;
+
+    public Account() {
+        this.balance = 0;
+        this.name = "";
+        this.personId = "";
+    }
+
+    public Account(String name, String personId) {
+        this.name = name;
+        this.personId = personId;
+        this.balance = 0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public void transfer(int value){
+        if(value>Account.transfLimit) return;
+        this.balance+=value;
+    }
+
+    public void print(){
+        System.out.println("Name: "+ this.name +". Ballance: $"+this.balance);
+        System.out.println("Transfer Limit: $"+ transfLimit);
+    }
+}
---
+++ src/br/com/manoellimadev/javastudying/oop/staticmodifier/test/Main.java
@@ -0,0 +1,24 @@
+package br.com.manoellimadev.javastudying.oop.staticmodifier.test;
+
+import br.com.manoellimadev.javastudying.oop.staticmodifier.domain.Account;
+
+public class Main {
+    public static void main(String[] args) {
+        Account acc01 = new Account("Manoel", "111111-11");
+        Account acc02 = new Account("Maria", "222222-22");
+
+        acc01.print();
+        System.out.println();
+        acc02.print();
+        System.out.println();
+//        System.out.println("Global transfer limit: $"+Account.transfLimit);
+        Account.transfLimit = 1900.50;
+//        System.out.println("Global transfer limit: $"+Account.transfLimit);
+//        System.out.println();
+        acc01.print();
+        System.out.println();
+        acc02.print();
+        System.out.println();
+
+    }
+}
