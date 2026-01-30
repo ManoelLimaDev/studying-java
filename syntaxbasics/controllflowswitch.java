6b34c4a59a290753d4d471d1283aa9f08361decb
ManoelLimaDev <kkmanel.lima@gmail.com>
2026-01-30 19:02:47 -0300

    feat: add control flow switch

--- syntaxbasics/controllflowswitch.java
+++ syntaxbasics/controllflowswitch.java
@@ -1,2 +1,41 @@
 public class controllflowswitch {
+    public static void main(String[] args) {
+        int day = 3;
+        System.out.println("The day number is: " + day);
+        switch (day){
+            case 1:
+                System.out.println("Today is Sunday");
+                break;
+
+            case 2:
+                System.out.println("Today is Monday");
+                break;
+
+            case 3:
+                System.out.println("Today is Tuesday");
+                break;
+
+            case 4:
+                System.out.println("Today is Wednsday");
+                break;
+
+            case 5:
+                System.out.println("Today is Thursday");
+                break;
+
+            case 6:
+                System.out.println("Today is Friday");
+                break;
+
+            case 7:
+                System.out.println("Today is Saturday");
+                break;
+
+            default:
+                System.out.println("Invalid day" + day);
+                System.out.println("Pick a day between 1 and 7");
+                break;
+        }
+
+    }
 }
