87fb5972b4376fe5478566829cc33a3a8c147797
ManoelLimaDev <kkmanel.lima@gmail.com>
2026-01-30 19:02:47 -0300

    feat: add primitive types examples

---
+++ syntaxbasics/PrimitivesTypes.java
@@ -0,0 +1,23 @@
+public class PrimitivesTypes {
+    public static void main(String[] args) {
+        int numI = 100000;
+        long numL = 1000000000L;
+        short numS = 10;
+        float numF = 100.52F;
+        double numD = 100.52;
+        byte numB = 10;
+        boolean isTrue = true;
+        char letter = 'W'; //ASCII = 87
+        String text = "Hello, World!!!";
+
+        System.out.println("Integer: "+ numI);
+        System.out.println("Long: "+ numL);
+        System.out.println("Short: "+ numS);
+        System.out.println("Float: "+ numF);
+        System.out.println("Double: "+ numD);
+        System.out.println("Byte: "+ numB);
+        System.out.println("Boolean: "+ isTrue);
+        System.out.println("Character: "+ letter);
+        System.out.println("String: "+ text);
+    }
+}
