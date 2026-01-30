c4c7bb932a78a47f8c4ee4eabd4b800f20a08193
ManoelLimaDev <kkmanel.lima@gmail.com>
2026-01-30 19:02:47 -0300

    Update and rename controlflowif.java to controlFlowIf.java

--- syntaxbasics/controlFlowIf.java
+++ syntaxbasics/controlFlowIf.java
@@ -1,4 +1,4 @@
-public class controlflowif {
+public class controlFlowIf {
     public static void main(String[] args) {
         int age = 10;
         System.out.println("Your age is: " + age + " years old.");
